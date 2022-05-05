c1ec31b79755c30a325e2988168e966522f5ed1a
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-05-04 21:58:51 -0300

    fix: Corrigido problema de número maximo de linhas e funções

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,13 +6,13 @@
 /*   By: guda-sil@student.42sp.org.br <guda-sil@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:02:24 by guda-sil@st       #+#    #+#             */
-/*   Updated: 2022/05/04 21:52:24 by guda-sil@st      ###   ########.fr       */
+/*   Updated: 2022/05/04 21:58:02 by guda-sil@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static	void	ft_lstadd_back(t_list **lst, t_list *new);
+// static	void	ft_lstadd_back(t_list **lst, t_list *new);
 static	void	read_and_store(int fd, t_list **acc, char *buffer);
 static	char	*create_line(t_list **acc);
 
@@ -51,28 +51,25 @@ static	void	read_and_store(int fd, t_list **acc, char *buffer)
 {
 	int		readed;
 	char	*temp;
-	t_list	*node;
 
-	node = *acc;
 	readed = read(fd, buffer, BUFFER_SIZE);
 	if (readed > 0)
 	{
-		if (node == NULL)
+		if ((*acc) == NULL)
 		{
-			node = (t_list *)malloc(sizeof(t_list));
-			ft_lstadd_back(acc, node);
-			node->content = ft_strdup("");
+			(*acc) = (t_list *)malloc(sizeof(t_list));
+			(*acc)->content = ft_strdup("");
 		}
 		buffer[readed] = '\0';
-		temp = node->content;
-		node->content = ft_strjoin(node->content, buffer);
+		temp = (*acc)->content;
+		(*acc)->content = ft_strjoin((*acc)->content, buffer);
 		free(temp);
-		while (readed > 0 && !ft_strchr(node->content, '\n'))
+		while (readed > 0 && !ft_strchr((*acc)->content, '\n'))
 		{
 			readed = read(fd, buffer, BUFFER_SIZE);
 			buffer[readed] = '\0';
-			temp = node->content;
-			node->content = ft_strjoin(node->content, buffer);
+			temp = (*acc)->content;
+			(*acc)->content = ft_strjoin((*acc)->content, buffer);
 			free(temp);
 		}
 	}
@@ -96,17 +93,17 @@ static	char	*create_line(t_list **acc)
 	return (line);
 }
 
-static	void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*node;
+// static	void	ft_lstadd_back(t_list **lst, t_list *new)
+// {
+// 	t_list	*node;
 
-	if ((*lst) == NULL)
-		(*lst) = new;
-	else
-	{
-		node = *lst;
-		while (node->next != NULL)
-			node = node->next;
-		node->next = new;
-	}
-}
+// 	if ((*lst) == NULL)
+// 		(*lst) = new;
+// 	else
+// 	{
+// 		node = *lst;
+// 		while (node->next != NULL)
+// 			node = node->next;
+// 		node->next = new;
+// 	}
+// }
